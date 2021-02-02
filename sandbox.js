5a6ee90b2f0d184f20ad4eb5e01ff84e01db31bc
Adrian Pabe <adrianpabs14@gmail.com>
2021-02-03 03:16:38 +0800

    Log JSON data

--- sandbox.js
+++ sandbox.js
@@ -5,14 +5,16 @@ const getTodos = (callback) => {
         // console.log(request, request.readyState);
         if (request.readyState === 4 && request.status === 200) {
             // console.log(request.responseText);
-            callback(undefined, request.responseText);
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
         } else if (request.readyState === 4) {
             // console.log('could not fetch the data');
             callback('could not fetch data', undefined);
         }
     })
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', 'todos.json');
     request.send();
 }
 
