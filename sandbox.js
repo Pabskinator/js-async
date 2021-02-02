2aa5bbd5b9209d7a0daa18e6aaed1f6f67c630d4
Adrian Pabe <adrianpabs14@gmail.com>
2021-02-03 04:16:11 +0800

    Log promises

--- sandbox.js
+++ sandbox.js
@@ -1,46 +1,88 @@
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest(); // old data format before json
+const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest(); // old data format before json
 
-    request.addEventListener('readystatechange', () => {
-        // console.log(request, request.readyState);
-        if (request.readyState === 4 && request.status === 200) {
-            // console.log(request.responseText);
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            // console.log('could not fetch the data');
-            callback('could not fetch data', undefined);
-        }
-    })
+        request.addEventListener('readystatechange', () => {
+            // console.log(request, request.readyState);
+            if (request.readyState === 4 && request.status === 200) {
+                // console.log(request.responseText);
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                // console.log('could not fetch the data');
+                reject('error getting results');
+            }
+        })
 
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.open('GET', resource);
-    request.send();
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+        request.open('GET', resource);
+        request.send();
+    });
 }
 
-// demonstrating async js
+// // demonstrating async js
+// console.log(1);
+// console.log(2);
+//
+// // callback hell - avoid this!
+// getTodos( 'todos/pabs.json', (err, data) => {
+//     console.log('callback fired');
+//
+//     getTodos('todos/gab.json', (err, data) => {
+//         console.log(data);
+//
+//         getTodos('todos/ruds.json', (err, data) => {
+//             console.log(data);
+//         })
+//     })
+//
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// });
+//
+// console.log(3);
+// console.log(4);
 
-console.log(1);
-console.log(2);
+// **************************************************************************************************************** //
 
-getTodos( 'todos/pabs.json', (err, data) => {
-    console.log('callback fired');
+// PROMISES
 
-    getTodos('todos/gab.json', (err, data) => {
-        console.log(data);
+// basic example of promise
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//         // fetch something
+//         resolve('some data');
+//         // reject('some error');
+//     });
+// };
 
-        getTodos('todos/ruds.json', (err, data) => {
-            console.log(data);
-        })
-    })
+// traditional way
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
 
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
+// other way of writing promises
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// })
 
-console.log(3);
-console.log(4);
 
+// then catch - chaining promises
+getTodos('todos/pabs.json').then(data => {
+    console.log('promise 1 resolved: ', data);
+    return getTodos('todos/gab.json');
+}).then(data => {
+    console.log('promise 2 resolved:', data)
+    return getTodos('todos/ruds.json');
+}).then(data => {
+    console.log('promise 3s resolved:', data)
+}).catch(err => {
+    console.log('promise rejected: ', err);
+})
