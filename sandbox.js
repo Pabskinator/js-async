45d17693dc375277234f44b94e7f0259ca959ee5
Adrian Pabe <adrianpabs14@gmail.com>
2021-02-03 03:04:34 +0800

    Log callback functions

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,35 @@
-const request = new XMLHttpRequest(); // old data format before json
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest(); // old data format before json
 
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText);
-    } else if (request.readyState === 4) {
-        console.log('could not fetch the data');
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState);
+        if (request.readyState === 4 && request.status === 200) {
+            // console.log(request.responseText);
+            callback(undefined, request.responseText);
+        } else if (request.readyState === 4) {
+            // console.log('could not fetch the data');
+            callback('could not fetch data', undefined);
+        }
+    })
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+}
+
+// demonstrating async js
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+    console.log('callback fired');
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
-})
+});
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
-request.send();
+console.log(3);
+console.log(4);
 
